b0815e902ef6ab4ec59679b6f5131627102a0cb2
luger-mano <lucas.germanysantos@gmail.com>
2025-10-09 02:20:52 -0300

    Coloquei os atributos necessários para ViaCepAPI

---
+++ Back-end/croche/src/main/java/com/vovo/croche/model/Address.java
@@ -0,0 +1,29 @@
+package com.vovo.croche.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Address {
+
+    private String cep;
+    private String logradouro;
+    private String complemento;
+    private String unidade;
+    private String bairro;
+    private String localidade;
+    private String uf;
+    private String estado;
+    private String regiao;
+    private String ibge;
+    private String gia;
+    private String ddd;
+    private String siafi;
+
+}
