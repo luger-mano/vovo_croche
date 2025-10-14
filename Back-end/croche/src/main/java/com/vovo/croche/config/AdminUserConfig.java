6369534217dbbaf36b9e51417a65b0ce577ea6d9
luger-mano <lucas.germanysantos@gmail.com>
2025-10-14 01:57:24 -0300

    Configuração de beans para security

---
+++ Back-end/croche/src/main/java/com/vovo/croche/config/AdminUserConfig.java
@@ -0,0 +1,46 @@
+package com.vovo.croche.config;
+
+import com.vovo.croche.model.Role;
+import com.vovo.croche.model.User;
+import com.vovo.croche.repository.RoleRepository;
+import com.vovo.croche.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.Set;
+
+@Configuration
+public class AdminUserConfig implements CommandLineRunner {
+
+    private final RoleRepository roleRepository;
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    public AdminUserConfig(RoleRepository roleRepository, UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
+        this.roleRepository = roleRepository;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    @Transactional
+    public void run(String... args) throws Exception {
+        var roleAdmin = roleRepository.findByName(Role.Values.ADMIN.name());
+        var userAdmin = userRepository.findByFullName("admin");
+
+        userAdmin.ifPresentOrElse(
+                user -> {
+                    System.out.println("Admin já existe.");
+                },
+                () -> {
+                    var user = new User();
+                    user.setFullName("admin");
+                    user.setPassword(passwordEncoder.encode("4577"));
+                    user.setRoles(Set.of(roleAdmin));
+                    userRepository.save(user);
+                }
+        );
+    }
+}
