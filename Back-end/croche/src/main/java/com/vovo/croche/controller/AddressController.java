fea9f3d62ea5b6d54c97edbd99aa6dacabe33dd2
luger-mano <lucas.germanysantos@gmail.com>
2025-10-24 19:17:52 -0300

    Adicionei novo método Get para buscar endereço pelo CEP

--- Back-end/croche/src/main/java/com/vovo/croche/controller/AddressController.java
+++ Back-end/croche/src/main/java/com/vovo/croche/controller/AddressController.java
@@ -1,17 +1,21 @@
 package com.vovo.croche.controller;
 
 
+import com.vovo.croche.model.dto.address.AddressCepRequestDTO;
 import com.vovo.croche.model.dto.address.AddressRequestDTO;
 import com.vovo.croche.model.dto.address.AddressResponseDTO;
 import com.vovo.croche.service.AddressService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.util.List;
 
 @RestController
-@RequestMapping("/addresses")
+@RequestMapping("api/v1/addresses")
 public class AddressController {
 
     private final AddressService service;
@@ -21,10 +25,14 @@ public class AddressController {
     }
 
     @GetMapping("/search-by-address")
-    public ResponseEntity<List<AddressResponseDTO>> searchForCepsThroughTheStreets(@RequestBody AddressRequestDTO dto) throws IOException, InterruptedException {
+    public ResponseEntity<List<AddressResponseDTO>> searchForCepsThroughTheStreets(@RequestBody AddressRequestDTO dto) {
 
         List<AddressResponseDTO> address = service.searchForCepsThroughTheStreets(dto);
-
         return ResponseEntity.ok(address);
+    }
+
+    @GetMapping("/search-cep")
+    public ResponseEntity<AddressResponseDTO> getAddressByCep(@RequestBody AddressCepRequestDTO dto) throws IOException, InterruptedException {
+        return ResponseEntity.ok(service.searchAddressByCep(dto));
     }
 }
